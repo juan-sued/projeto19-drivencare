61f7699f32496990e244b2c82a667de61e522b52
Juan Sued <juansued19@gmail.com>
2023-03-29 23:57:34 -0300

    created db and config

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import bookRoutes from "./bookRoutes.js";
+import consultRoutes from "./consultRoutes.js";
 import userRoutes from "./userRoutes.js";
 
 const routes = Router();
 
 routes.use("/users", userRoutes);
-routes.use("/books", bookRoutes);
+routes.use("/consults", consultRoutes);
 
 export default routes;
